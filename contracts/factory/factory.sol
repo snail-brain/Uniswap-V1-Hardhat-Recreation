120d2d5b98b5a782bd8f2c3ceb66194ea506f581
snail-brain <snail-brain@protonmail.com>
2022-05-23 16:53:56 -0500

    create and test factory contract

--- contracts/factory/factory.sol
+++ contracts/factory/factory.sol
@@ -1,5 +1,25 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "../exchange/exchange.sol";
 
-contract Factory {}
+contract Factory {
+    mapping(address => address) public tokenToExchange;
+
+    function createExchange(address _token) public returns (address) {
+        require(_token != address(0), "invalid token address");
+        require(
+            tokenToExchange[_token] == address(0),
+            "exchange already exists!"
+        );
+
+        Exchange exchange = new Exchange(_token);
+        tokenToExchange[_token] = address(exchange);
+
+        return address(exchange);
+    }
+
+    function getExchange(address _token) public view returns (address) {
+        return tokenToExchange[_token];
+    }
+}
--- tests/factory.test.ts
+++ tests/factory.test.ts
@@ -1,7 +1,11 @@
 import "@nomiclabs/hardhat-waffle";
 import * as helper from "../scripts/helpful_scripts";
+import { expect } from "chai";
 import { ethers } from "hardhat";
-import { BigNumber } from "ethers";
+import { BigNumber, Contract, Signer } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+
 
 const getBalance = ethers.provider.getBalance;
 const toWei = (value: any) =>
@@ -10,14 +14,25 @@ const fromWei = (value: any) =>
     ethers.utils.formatEther(BigNumber.from(value.toString()));
 
 describe("Factory", async () => {
-    const owner = ethers.getSigners();
-    const Factory = helper.deployContract("Factory");
-
-
+    let owner: SignerWithAddress;
+    let user: SignerWithAddress;
+    let factory: Contract;
+    let token: Contract;
 
     beforeEach(async () => {
+        [owner, user] = await ethers.getSigners();
+        factory = await helper.deployContract("Factory");
+        token = await helper.deployContract("Token", "Snail", "SNL", toWei('10000'));
 
     });
 
 
+    describe("createExchange", async () => {
+        it("Deploys New Exchange", async () => {
+            await helper.waitForTx(await factory.createExchange(token.address));
+            const factoryAddress = await factory.callStatic.getExchange(token.address);
+            expect(factoryAddress).to.not.equal(ethers.constants.AddressZero);
+            console.log(factoryAddress);
+        });
+    });
 });
